add085e77bb6a97a71242a29196250465cf7a55a
maria-otero <m.jesuotero@gmail.com>
2018-08-09 15:31:23 -0400

    problems with the portfolio nav filter, so I removed it

--- js/main.js
+++ js/main.js
@@ -92,17 +92,17 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
+  // var portfolioIsotope = $('.portfolio-container').isotope({
+  //   itemSelector: '.portfolio-thumbnail',
+  //   layoutMode: 'fitRows'
+  // });
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-filters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  // $('#portfolio-flters li').on( 'click', function() {
+  //   $("#portfolio-filters li").removeClass('filter-active');
+  //   $(this).addClass('filter-active');
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
+  //   portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  // });
 
 
   // ========================================================================= //
