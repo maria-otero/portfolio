b2ef9bbcbfb2716ef272d9c86529d3be2bc37d1b
maria-otero <m.jesuotero@gmail.com>
2018-08-16 00:03:44 -0300

    clean img folder

--- js/main.js
+++ js/main.js
@@ -87,23 +87,6 @@ $(document).ready(function() {
   });
 
 
-  // ========================================================================= //
-  //  Porfolio isotope and filter
-  // ========================================================================= //
-
-
-  // var portfolioIsotope = $('.portfolio-container').isotope({
-  //   itemSelector: '.portfolio-thumbnail',
-  //   layoutMode: 'fitRows'
-  // });
-
-  // $('#portfolio-flters li').on( 'click', function() {
-  //   $("#portfolio-filters li").removeClass('filter-active');
-  //   $(this).addClass('filter-active');
-
-  //   portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  // });
-
 
   // ========================================================================= //
   //  OwlCarousel
@@ -128,38 +111,5 @@ $(document).ready(function() {
     }
 });
 
-  // ========================================================================= //
-  //  magnificPopup
-  // ========================================================================= //
-
-  // var magnifPopup = function() {
-  //   $('.popup-img').magnificPopup({
-  //     type: 'image',
-  //     removalDelay: 300,
-  //     mainClass: 'mfp-with-zoom',
-  //     gallery: {
-  //       enabled: false
-  //     },
-  //     zoom: {
-  //       enabled: true, // By default it's false, so don't forget to enable it
-
-  //       duration: 300, // duration of the effect, in milliseconds
-  //       easing: 'ease-in-out', // CSS transition easing function
-
-  //       // The "opener" function should return the element from which popup will be zoomed in
-  //       // and to which popup will be scaled down
-  //       // By defailt it looks for an image tag:
-  //       opener: function(openerElement) {
-  //         // openerElement is the element on which popup was initialized, in this case its <a> tag
-  //         // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-  //         return openerElement.is('img') ? openerElement : openerElement.find('img');
-  //       }
-  //     }
-  //   });
-  // };
-
-
-  // // Call the functions
-  // magnifPopup();
 
 });
